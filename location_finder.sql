f8cc8cfe4f9ed7437c89836d93fd5ed49ed045cb
Garin J Tanner <32025331+GarinJTanner@users.noreply.github.com>
2022-06-21 08:29:50 -0700

    Update location_finder.sql

--- location_finder.sql
+++ location_finder.sql
@@ -8,5 +8,6 @@ SET @lat2 = (SELECT latitude FROM usa WHERE id = @city_id),
     @long2= (SELECT longitude FROM usa WHERE id = @city_id);
 SELECT (
 	SELECT city FROM usa WHERE id = @city_id) as city,(
+   	SELECT zip FROM usa WHERE id = @city_id) as zip,(
 	SELECT state FROM usa WHERE id = @city_id) as state,(
  	SELECT name FROM counties WHERE st_contains(the_geom,@point)) as county
