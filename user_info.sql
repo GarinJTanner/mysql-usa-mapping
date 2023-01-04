e56238db7dfe75fe03bbaa4108e6780c143b9e3a
Garin J Tanner <32025331+GarinJTanner@users.noreply.github.com>
2023-01-04 14:59:01 -0800

    Update user_info.sql

--- user_info.sql
+++ user_info.sql
@@ -1,12 +1,23 @@
-set @user_id = 3;
+set @user_id = 51;
 set @lat=(SELECT latitude FROM users WHERE id=@user_id),@long=(SELECT longitude FROM users WHERE id=@user_id);
+set @city_id = (select id from (SELECT *,
+	
+( 3959 * acos( cos( radians(@lat) ) * cos( radians( latitude ) ) 
+* cos( radians( longitude ) - radians(@long) )
+ + sin( radians(@lat) ) * sin(radians(latitude)) ) ) 
+			AS distance 
+
+FROM usa 
+
+HAVING distance < 50
+ORDER BY distance limit 1)as t);
 SELECT 
 (SELECT username FROM users WHERE id = @user_id) as username,(
 SELECT ip_address FROM users WHERE id = @user_id) as ip_address,(
 SELECT city FROM usa WHERE id = @city_id) as city,(
-SELECT zip FROM usa WHERE id = @city_id) as zip,(
 SELECT state FROM usa WHERE id = @city_id) as state,(
-SELECT UPPER(name) FROM counties WHERE st_contains(the_geom,@point)) as county,(
+SELECT zip FROM usa WHERE id = @city_id) as zip,(
+SELECT UPPER(name) FROM counties WHERE st_contains(the_geom,point(@long,@lat))) as county,(
 SELECT latitude FROM users WHERE id = @user_id) as latitude,(
 SELECT longitude FROM users WHERE id = @user_id) as longitude,(
 SELECT phone_model FROM devices WHERE id in (SELECT device_id FROM user_devices WHERE user_id = @user_id)) as device,(
@@ -32,6 +43,6 @@ SELECT truncate(distance,3) FROM (SELECT *,
 
 FROM locations 
 
-HAVING distance < 50
+HAVING distance < 500
 ORDER BY distance 
 LIMIT 1)as distance) as shake_shack_distance
