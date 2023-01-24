34ecf886956c17f9343c1bfd333eb9400b9be34e
Garin J Tanner <32025331+GarinJTanner@users.noreply.github.com>
2023-01-23 21:17:44 -0800

    Update usergen.sql

--- usergen.sql
+++ usergen.sql
@@ -1,4 +1,3 @@
-use papa;
 # random number generator (syntax: random(x,y)  Ex: select random(1,100))
 drop function if exists random_int;
 DELIMITER //
@@ -67,35 +66,3 @@ END WHILE;
 END $$;
 DELIMITER ;
 call usertable;
-
-drop procedure if exists gps_from_zip;
-DELIMITER $$;
-CREATE PROCEDURE gps_from_zip()
-
-BEGIN
-DECLARE $user_count,$user_int INT default 1;
-DECLARE $city,$zip,$state,$county varchar(30);
-DECLARE $lat decimal(11,4);
-DECLARE $long decimal(12,4);
-
-set $user_count = (select count(id) from users),
-	$user_int   = (select id from users group by id order by id asc limit 1);
-
-myloop : loop
-
-set $zip = (select zip from users where id = $user_int);
-set $lat = (select latitude from usa where zip = $zip),
-	$long = (select longitude from usa where zip = $zip),
-    $city = (select city from usa where zip = $zip),
-	$state = (select state from usa where zip = $zip);
-set $county = (SELECT (name) FROM counties WHERE st_contains(the_geom,point($long,$lat)));
-
-update users set latitude = $lat, longitude = $long, city = $city, county = $county, state = $state where id = $user_int;
-
-set $user_int = $user_int + 1;	      
-IF $user_int = $user_count + 1 THEN leave myloop;END IF;
-end loop myloop;
-
-END $$;
-DELIMITER ;
-call gps_from_zip;
